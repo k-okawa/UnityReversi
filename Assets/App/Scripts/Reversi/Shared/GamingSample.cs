fef0d6daaa244f40b79b82f7a530acac0954be64
Kyohei <kurokenken0310@gmail.com>
2023-01-19 00:12:46 +0900

    implement server side streaming hub

---
+++ Assets/App/Scripts/Reversi/Shared/GamingSample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using MagicOnion;
+using MessagePack;
+using UnityEngine;
+
+namespace App.Reversi.Shared
+{
+    // Server -> Client definition
+    public interface IGamingHubReceiver
+    {
+        // The method must have a return type of `void` and can have up to 15 parameters of any type.
+        void OnJoin(Player player);
+        void OnLeave(Player player);
+        void OnMove(Player player);
+    }
+
+    // Client -> Server definition
+    // implements `IStreamingHub<TSelf, TReceiver>`  and share this type between server and client.
+    public interface IGamingHub : IStreamingHub<IGamingHub, IGamingHubReceiver>
+    {
+        // The method must return `ValueTask`, `ValueTask<T>`, `Task` or `Task<T>` and can have up to 15 parameters of any type.
+        ValueTask<Player[]> JoinAsync(string roomName, string userName, Vector3 position, Quaternion rotation);
+        ValueTask LeaveAsync();
+        ValueTask MoveAsync(Vector3 position, Quaternion rotation);
+    }
+
+    // for example, request object by MessagePack.
+    [MessagePackObject]
+    public class Player
+    {
+        [Key(0)]
+        public string Name { get; set; }
+        [Key(1)]
+        public Vector3 Position { get; set; }
+        [Key(2)]
+        public Quaternion Rotation { get; set; }
+    }
+}
---
+++ Server/Server/Services/GamingHub.cs
@@ -0,0 +1,43 @@
+﻿using App.Reversi.Shared;
+using MagicOnion.Server.Hubs;
+using UnityEngine;
+
+namespace UnityReversi.Server.Services
+{
+    public class GamingHub : StreamingHubBase<IGamingHub, IGamingHubReceiver>, IGamingHub
+    {
+        private IGroup room;
+        private Player self;
+        private IInMemoryStorage<Player> storage;
+
+        public async ValueTask<Player[]> JoinAsync(string roomName, string userName, Vector3 position, Quaternion rotation)
+        {
+            self = new Player() { Name = userName, Position = position, Rotation = rotation };
+
+            (room, storage) = await Group.AddAsync(roomName, self);
+            
+            Broadcast(room).OnJoin(self);
+
+            return storage.AllValues.ToArray();
+        }
+
+        public async ValueTask LeaveAsync()
+        {
+            await room.RemoveAsync(Context);
+            Broadcast(room).OnLeave(self);
+        }
+
+        public ValueTask MoveAsync(Vector3 position, Quaternion rotation)
+        {
+            self.Position = position;
+            self.Rotation = rotation;
+            Broadcast(room).OnMove(self);
+            return ValueTask.CompletedTask;
+        }
+
+        protected override ValueTask OnDisconnected()
+        {
+            return ValueTask.CompletedTask;
+        }
+    }
+}
